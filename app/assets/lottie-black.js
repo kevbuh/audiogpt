7438759993ca5323aade24a87d6af7ea4b7604af
Kevin Buhler <buhlerkw@gmail.com>
2023-03-05 10:25:13 -0700

    updated ui with savvy

--- app/assets/lottie-black.js
+++ app/assets/lottie-black.js
@@ -6,8 +6,12 @@ export default class LottieBlack extends React.Component {
     return (
       <LottieView
         source={require("app/assets/120096-ai-assistant-animation.json")}
+        // source={require("app/assets/120096-ai-assistant-animation.json")}
+
         
         style={{ width: 400, height: 400 }}
+        // style={{ width: 400, height: 400 }}
+
         autoPlay
       />
     );
