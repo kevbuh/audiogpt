a8017ae99cd4ef6d9e47487da399a202c7722f64
Kevin Buhler <buhlerkw@gmail.com>
2023-03-05 11:40:52 -0700

    final changes

--- app/assets/lottie-black.js
+++ app/assets/lottie-black.js
@@ -5,12 +5,13 @@ export default class LottieBlack extends React.Component {
   render() {
     return (
       <LottieView
+        speed={0.4}
+        // source={require("app/assets/wfMURAhQQ6.json")}
         source={require("app/assets/120096-ai-assistant-animation.json")}
-        // source={require("app/assets/120096-ai-assistant-animation.json")}
 
         
+        // style={{ width: 200, height: 200 }}
         style={{ width: 400, height: 400 }}
-        // style={{ width: 400, height: 400 }}
 
         autoPlay
       />
